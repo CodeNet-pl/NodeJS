c4709a67410084d5c0eab3229be23d0e1fb9ff96
Bartosz Pasiński <b.pasinski@codenet.pl>
2025-07-14 14:51:06 +0000

    feat(hal-api): override protocol, host and prefix

--- packages/hal-api/src/express.ts
+++ packages/hal-api/src/express.ts
@@ -1,12 +1,14 @@
 import { NextFunction, Request, Response } from 'express';
 import { withBaseUrl } from './hook';
 
-export function halLinks(req: Request, res: Response, next: NextFunction) {
-  withBaseUrl(
-    (req.get('X-Forwarded-Proto') ?? req.protocol) +
-      '://' +
-      (req.get('X-Forwarded-Host') ?? req.get('host')) +
-      (req.get('X-Forwarded-Prefix') ?? '/'),
-    next
-  );
-}
+export const halLinks =
+  (options?: { protocol?: 'http' | 'https'; host?: string; prefix?: string }) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    withBaseUrl(
+      (options?.protocol ?? req.get('X-Forwarded-Proto') ?? req.protocol) +
+        '://' +
+        (options?.host ?? req.get('X-Forwarded-Host') ?? req.get('host')) +
+        (options?.prefix ?? req.get('X-Forwarded-Prefix') ?? '/'),
+      next
+    );
+  };
